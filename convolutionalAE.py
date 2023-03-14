d4c67f9ee0e4bf8eb69547b12a6316ad673f623d
Leon Scharwächter <56418155+lscharwaechter@users.noreply.github.com>
2023-03-14 15:36:39 +0100

    Add files via upload

---
+++ convolutionalAE.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Leon Scharwächter
+"""
+
+from torch import Tensor
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
+
+''' 
+-- Notes --
+Checkerboard effekt:
+Use a kernel size that is divided by the stride,
+avoiding the overlap issue.
+
+If not using ConvTranspose1d:
+each UpSampling2D layer must be followed by a Conv2D layer
+that will learn to interpret the doubled input and be trained
+to translate it into meaningful detail.
+'''
+
+class ConvolutionalAE(nn.Module):
+    def __init__(self,
+                 maxlen: int,
+                 num_variables: int,
+                 d_latent: int):
+        super(ConvolutionalAE, self).__init__()
+        self.d_latent = d_latent
+
+        ##############################################
+        ###               ENCODER-PART             ###
+        ##############################################
+        
+        self.conv1_enc = nn.Conv1d(in_channels=1, out_channels=2, kernel_size=21, 
+                                   stride=1, padding=10, padding_mode='replicate')
+        self.conv2_enc = nn.Conv1d(in_channels=2, out_channels=4, kernel_size=18, 
+                                   stride=1, padding=8, padding_mode='replicate')
+        self.conv3_enc = nn.Conv1d(in_channels=4, out_channels=8, kernel_size=15, 
+                                   stride=1, padding=7, padding_mode='replicate')
+        self.conv4_enc = nn.Conv1d(in_channels=8, out_channels=16, kernel_size=13, 
+                                   stride=1, padding=6, padding_mode='replicate')
+        self.conv5_enc = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=11, 
+                                   stride=1, padding=5, padding_mode='replicate')
+        self.conv6_enc = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=8,
+                                   stride=1, padding=3, padding_mode='replicate')
+        self.conv7_enc = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, 
+                                   stride=1, padding=2, padding_mode='replicate')
+        self.conv8_enc = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, 
+                                   stride=1, padding=1, padding_mode='replicate')
+        ## Aggregation layer
+        self.conv9_enc = nn.Conv1d(in_channels=256, out_channels=1, kernel_size=3,
+                                   padding=2, stride=1)
+        self.dense_enc = nn.Linear(maxlen*num_variables, d_latent)
+        
+        ##############################################
+        ###               DECODER-PART             ###
+        ##############################################
+        
+        # From the latent space to hidden convolution size
+        self.dense_dec = nn.Linear(d_latent, maxlen*num_variables)
+        # De-Aggregation layer (1->256)
+        self.conv1_dec = nn.ConvTranspose1d(in_channels=1, out_channels=256, kernel_size=3,
+                                            padding=2, stride=1)
+        self.conv2_dec = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=3,
+                                            padding=1, stride=1)
+        self.conv3_dec = nn.ConvTranspose1d(in_channels=128, out_channels=64, kernel_size=5,
+                                            padding=2, stride=1)
+        self.conv4_dec = nn.ConvTranspose1d(in_channels=64, out_channels=32, kernel_size=8,
+                                            padding=3, stride=1)
+        self.conv5_dec = nn.ConvTranspose1d(in_channels=32, out_channels=16, kernel_size=11,
+                                            padding=5, stride=1)
+        self.conv6_dec = nn.ConvTranspose1d(in_channels=16, out_channels=8, kernel_size=13,
+                                            padding=6, stride=1)
+        self.conv7_dec = nn.ConvTranspose1d(in_channels=8, out_channels=4, kernel_size=15,
+                                            padding=7, stride=1)
+        self.conv8_dec = nn.ConvTranspose1d(in_channels=4, out_channels=2, kernel_size=18,
+                                            padding=8, stride=1)
+        self.conv9_dec = nn.ConvTranspose1d(in_channels=2, out_channels=1, kernel_size=21,
+                                            padding=10, stride=1)
+        
+        ## Bound the activation
+        #self.Tanh = nn.Tanh()
+        #self.ReLU = nn.ReLU()
+        #self.ELU = nn.ELU()()
+        
+        # Best performance was achieved without the use of
+        # an activation function 
+        
+    def encode(self, timeseries: Tensor):
+        # Encode timeseries into latent space
+        x = self.conv1_enc(timeseries)
+        x = self.conv2_enc(x)
+        x = self.conv3_enc(x)
+        x = self.conv4_enc(x)
+        x = self.conv5_enc(x)
+        x = self.conv6_enc(x)
+        x = self.conv7_enc(x)
+        x = self.conv8_enc(x)
+        x = self.conv9_enc(x)
+        x = self.dense_enc(x)
+        return x
+    
+    def decode(self, latentpoint: Tensor):
+        # Decode latent point back into a time series
+        x = self.dense_dec(latentpoint)
+        x = self.conv1_dec(x)
+        x = self.conv2_dec(x)
+        x = self.conv3_dec(x)
+        x = self.conv4_dec(x)
+        x = self.conv5_dec(x)
+        x = self.conv6_dec(x)
+        x = self.conv7_dec(x)
+        x = self.conv8_dec(x)
+        x = self.conv9_dec(x)
+        return x
+        
+    def forward(self, src_input: Tensor):
+        # Reshape input from (bz, seqlength, dim) to
+        # (bz, ch = 1, seqlength*dim) 
+        # as required input for the 1d-convolution layers
+        bz, seqlength, dim = src_input.shape
+        src_input = src_input.reshape((bz,1,seqlength*dim))
+        
+        latent_point = self.encode(src_input)
+        reconstruction = self.decode(latent_point)
+        
+        # Reshape flattened time series to the number of dimensions
+        return reconstruction.reshape((bz,seqlength,dim))
+    
+    def sample(self, bz: int = 1):
+        '''
+        Samples a random point from the latent space.
+        Currently, values are uniformly sampled 
+        from the interval [-1,1)
+        '''
+        point = torch.rand((bz, 1, self.d_latent),device=DEVICE)
+        upperl, lowerl = torch.ones(1,device=DEVICE)[0], -torch.ones(1,device=DEVICE)[0]
+        point = (lowerl - upperl)*point + upperl
+        return point.to(DEVICE)
+    
+class Discriminator(nn.Module):
+    def __init__(self,
+                 maxlen: int,
+                 num_variables: int,
+                 d_latent: int):
+        super(Discriminator, self).__init__()
+        
+        # Similar to the Encoder-Part of the Autoencoder
+        self.conv1_enc = nn.Conv1d(in_channels=1, out_channels=2, kernel_size=21, 
+                                   stride=1, padding=10, padding_mode='replicate')
+        self.conv2_enc = nn.Conv1d(in_channels=2, out_channels=4, kernel_size=18, 
+                                   stride=1, padding=8, padding_mode='replicate')
+        self.conv3_enc = nn.Conv1d(in_channels=4, out_channels=8, kernel_size=15, 
+                                   stride=1, padding=7, padding_mode='replicate')
+        self.conv4_enc = nn.Conv1d(in_channels=8, out_channels=16, kernel_size=13, 
+                                   stride=1, padding=6, padding_mode='replicate')
+        self.conv5_enc = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=11, 
+                                   stride=1, padding=5, padding_mode='replicate')
+        self.conv6_enc = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=8, 
+                                   stride=1, padding=3, padding_mode='replicate')
+        self.conv7_enc = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, 
+                                   stride=1, padding=2, padding_mode='replicate')
+        self.conv8_enc = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, 
+                                   stride=1, padding=1, padding_mode='replicate')
+        ## Aggregation layer
+        self.conv9_enc = nn.Conv1d(in_channels=256, out_channels=1, kernel_size=3,
+                                   padding=2, stride=1)
+        
+        # Last layers
+        self.dense_enc = nn.Linear(maxlen*num_variables, d_latent) #612 #1224 #4096
+        self.dense_one = nn.Linear(d_latent,1)
+        self.sigmoid = nn.Sigmoid()
+        
+    def encode(self, timeseries: Tensor):
+        # Encode timeseries into prediction
+        x = self.conv1_enc(timeseries)
+        x = self.conv2_enc(x)
+        x = self.conv3_enc(x)
+        x = self.conv4_enc(x)
+        x = self.conv5_enc(x)
+        x = self.conv6_enc(x)
+        x = self.conv7_enc(x)
+        x = self.conv8_enc(x)
+        x = self.conv9_enc(x)
+        x = self.dense_enc(x)
+        x = self.dense_one(x)
+        #x = self.sigmoid(x)
+        return x
+        
+    def forward(self, src_input: Tensor):
+        bz, seqlength, dim = src_input.shape
+        src_input = src_input.reshape((bz,1,seqlength*dim))
+        return self.encode(src_input)
+        
---
+++ transformerGAN.py
@@ -0,0 +1,351 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Leon Scharwächter
+"""
+
+from torch import Tensor
+import torch
+import torch.nn as nn
+from torch.nn import Transformer
+import numpy as np
+import math
+
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
+
+class VarSeriesEmbedding(nn.Module):
+    '''
+    This class scales an input sequence, which is a tensor
+    of one time step, containing all variables at this position,
+    to the model dimension of the transformer (e.g. 512).
+    This scaling is done with learnable parameters of a 
+    1-dimensional feed forward neural network.
+    '''
+    def  __init__(self,
+                  num_variables: int,
+                  d_model: int):
+        super(VarSeriesEmbedding, self).__init__()
+        self.linear = nn.Linear(num_variables, d_model, bias=True)
+        
+    def forward(self, varSeries: Tensor):
+        return self.linear(varSeries)
+    
+class PositionalEncoding(nn.Module):
+    '''
+    This class adds a positional encoding to the sequence embedding 
+    to introduce a notion of time dependend order.
+    'influence' determines how far the position can have an effect 
+    on a token.
+    Returns the sum of the input embedding (bz, seqlength, d_model)
+    with the raw positional encoding (1, seqlength, d_model)
+    '''
+    def __init__(self,
+                 d_model: int,
+                 influence: int = 5000,
+                 dropout: float = 0.1):
+        super(PositionalEncoding, self).__init__()
+        den = torch.exp(- torch.arange(0, d_model, 2) * math.log(10000) / d_model)
+        pos = torch.arange(0, influence).reshape(influence, 1)
+        pos_embedding = torch.zeros((influence, d_model))
+        pos_embedding[:, 0::2] = torch.sin(pos * den)
+        pos_embedding[:, 1::2] = torch.cos(pos * den)[:,:int(d_model/2)]
+        
+        # Add batch dimension (influence,d_model) -> (1,influence,d_model)
+        pos_embedding = pos_embedding.unsqueeze(0)
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('pos_embedding', pos_embedding)
+
+    def forward(self, seq_embedding: Tensor):
+        encoding = self.pos_embedding[:,:seq_embedding.size(1),:]
+        return self.dropout(seq_embedding + encoding)
+
+class TransformerModel(nn.Module):
+    '''
+    The Transformer model serves as an Autoencoder.
+    Currently, the output of the Transformer-Encoder (memory)
+    is passed through a linear layer which acts as a bottleneck.
+    The output of the Transformer-Decoder is passed through
+    another linear layer to scale the output to the number of
+    target variables
+    '''
+    
+    def __init__(self,
+                 d_model: int,
+                 d_latent: int,
+                 maxlen: int,
+                 num_src_variables: int,
+                 num_tgt_variables: int,
+                 num_encoder_layers: int = 6,
+                 num_decoder_layers: int = 6,
+                 nhead: int = 8,
+                 dim_feedforward: int = 2048,
+                 dropout: float = 0.1):
+        super(TransformerModel, self).__init__()      
+        self.d_model = d_model
+        self.d_latent = d_latent
+        self.maxlen = maxlen
+        
+        # Initialize layers       
+        self.transformer = Transformer(d_model=d_model,
+                                       nhead=nhead,
+                                       num_encoder_layers=num_encoder_layers,
+                                       num_decoder_layers=num_decoder_layers,
+                                       dim_feedforward=dim_feedforward,
+                                       dropout=dropout,
+                                       batch_first=True)  
+        
+        self.latentEncoder = nn.Linear(maxlen*d_model, d_latent) 
+        self.Tanh = nn.Tanh()
+        self.latentDecoder = nn.Linear(d_latent, maxlen*d_model) 
+        
+        # Output linear layer
+        self.linear = nn.Linear(d_model, num_tgt_variables)
+        
+        # Initialize helper modules
+        self.src_embedding = VarSeriesEmbedding(num_src_variables, d_model)
+        self.tgt_embedding = VarSeriesEmbedding(num_tgt_variables, d_model)    
+        self.positional_encoding = PositionalEncoding(d_model)
+
+    def init_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self,
+                src_pad: Tensor,
+                tgt_pad: Tensor,
+                src_mask: Tensor,
+                tgt_mask: Tensor,
+                src_padding_mask: Tensor,
+                tgt_padding_mask: Tensor,
+                memory_key_padding_mask: Tensor):
+         
+        # Get (bottleneck) memory
+        memory = self.encode(src_pad,
+                             src_mask,
+                             src_padding_mask)
+        # Get output
+        outs = self.decode(tgt_pad,
+                           tgt_mask,
+                           None, # memory_mask
+                           tgt_padding_mask,
+                           memory)
+               
+        # Scale output dim to the number of variables of the dataset
+        outs = self.linear(outs)
+        
+        return outs
+    
+    def encode(self,
+               src_pad: Tensor,
+               src_mask: Tensor = None, 
+               src_padding_mask: Tensor = None):  
+        bz, _, _ = src_pad.shape
+        src_emb = torch.zeros((bz, self.maxlen, self.d_model),device=DEVICE) 
+        
+        # Encode input batch into the embedding
+        for batch in range(bz):
+            src_emb[batch,:] = torch.stack([self.src_embedding(i) for i in src_pad[batch]])
+        
+        # Add positional encoding
+        src_emb = self.positional_encoding(src_emb)
+        
+        # Get Transformer-Encoder output
+        memory = self.transformer.encoder(src = src_emb,
+                                          mask = src_mask,
+                                          src_key_padding_mask = src_padding_mask)
+        # Create Bottleneck
+        memory = self.latentEncoder(memory.reshape((bz, self.maxlen*self.d_model)))
+        memory = self.Tanh(memory)         
+
+        return memory
+     
+    def decode(self,
+               tgt: Tensor,
+               tgt_mask: Tensor, 
+               memory_mask: Tensor,
+               tgt_padding_mask: Tensor,
+               memory: Tensor):
+        bz, seqlength, _ = tgt.shape
+        tgt_emb = torch.zeros((bz, seqlength, self.d_model),device=DEVICE) 
+        
+        # Encode input batch into the embedding
+        for batch in range(bz):
+            tgt_emb[batch,:] = torch.stack([self.tgt_embedding(i) for i in tgt[batch]])
+        
+        # Add positional encoding
+        tgt_emb = self.positional_encoding(tgt_emb) 
+        
+        # Decompress bottleneck
+        memory_dec = self.latentDecoder(memory).reshape((bz, self.maxlen, self.d_model))
+        
+        return self.transformer.decoder(tgt = tgt_emb,
+                                        memory = memory_dec,
+                                        tgt_mask = tgt_mask,
+                                        memory_mask = memory_mask,
+                                        tgt_key_padding_mask = tgt_padding_mask)
+    
+    
+    def greedy_decode_bz(self,
+                         memory: Tensor,
+                         maxlen: int,
+                         num_variables: int,
+                         SOS: int):
+        '''
+        Decodes a batch of latent points into 
+        a time series without using a given masked output sequence
+        as during training. 
+        '''
+        bz = memory.shape[0]
+        ys = torch.ones((bz,1,num_variables),device=DEVICE)*SOS
+        
+        for i in range(maxlen-1): #-1 because SOS
+            # Generate square subsequent mask
+            tgt_mask = (torch.triu(torch.ones((ys.shape[1], ys.shape[1]), device=DEVICE)) == 1).transpose(0, 1)
+            tgt_mask = tgt_mask.float().masked_fill(tgt_mask == 0, float('-inf')).masked_fill(tgt_mask == 1, float(0.0))
+            tgt_mask.to(DEVICE)
+            
+            # Decode using the given memory and the current target+mask
+            out = self.decode(ys,
+                              tgt_mask = tgt_mask,
+                              memory_mask = None,
+                              tgt_padding_mask = None,
+                              memory = memory)
+            # Scale output to number of target variables
+            out = self.linear(out) # shape: (bz, seqlength, num_var)
+            
+            # Pick the last element of the prediction
+            # and append it to the target sequence
+            out = torch.narrow(out, 1, -1 ,1) # dim, start, length
+            ys = torch.cat([ys, out], dim=1)           
+        return ys
+    
+    def greedy_decode(self,
+                      memory: Tensor,
+                      maxlen: int,
+                      num_variables: int,
+                      SOS: int,
+                      EOS: int):
+        '''
+        Decodes a single point from the latent space (memory) into 
+        a time series without using a given masked output sequence
+        as during training. Here, varying sequence lengths are allowed
+        and the algorithm stops if maxlen or the End-Of-Sequence
+        EOS-Token is generated.
+        '''
+        #print('Start greedy_decode() ...')
+        
+        EOS_ = torch.ones((1,num_variables),device=DEVICE)*EOS  
+        ys = torch.ones((1,1,num_variables),device=DEVICE)*SOS
+        
+        # ys shape: (bz = 1, len = 1, num_variables)
+        for i in range(maxlen-1): #-1 because SOS, -2 if SOS+EOS          
+            # Generate square subsequent mask
+            tgt_mask = (torch.triu(torch.ones((ys.shape[1], ys.shape[1]), device=DEVICE)) == 1).transpose(0, 1)
+            tgt_mask = tgt_mask.float().masked_fill(tgt_mask == 0, float('-inf')).masked_fill(tgt_mask == 1, float(0.0))
+            tgt_mask.to(DEVICE)
+            
+            # Decode using the given memory and the current target+mask
+            out = self.decode(ys,
+                              tgt_mask = tgt_mask,
+                              memory_mask = None,
+                              tgt_padding_mask = None,
+                              memory = memory)
+            # Scale output to number of target variables
+            out = self.linear(out) # shape: (bz, seqlength, num_var)
+            
+            # Pick the last element of the prediction
+            # and append it to the target sequence
+            out = torch.narrow(out, 1, -1 ,1) # dim, start, length
+            ys = torch.cat([ys, out], dim=1)
+                 
+            if out == EOS_:  
+                break            
+        ys = torch.cat([ys, torch.FloatTensor(np.array([EOS_]))], dim=1)          
+        return ys
+    
+    def sample(self, bz: int = 1):
+        '''
+        Samples a random point from the latent space.
+        Currently, values are uniformly sampled 
+        from the interval [-1,1)
+        '''
+        memory = torch.rand((bz, self.d_latent),device=DEVICE)
+        upperl, lowerl = torch.ones(1,device=DEVICE)[0], -torch.ones(1,device=DEVICE)[0]
+        memory = (lowerl - upperl)*memory + upperl
+        return memory.to(DEVICE)
+    
+    
+class Discriminator(nn.Module):
+    '''
+    The Discriminator network of the GAN architecture
+    consists of a Transformer-Encoder
+    with the same hyperparameters as the Autoencoder-Transformer 
+    and a FFN projecting to only one neuron to make a binary prediction.
+    '''
+    def __init__(self,                
+                 d_model: int,
+                 d_latent: int,
+                 maxlen: int,
+                 num_src_variables: int,
+                 num_encoder_layers: int = 6,
+                 nhead: int = 8,
+                 dim_feedforward: int = 2048,
+                 dropout: float = 0.1):
+        
+        super(Discriminator, self).__init__()
+        self.d_model = d_model
+        self.d_latent = d_latent
+        self.maxlen = maxlen
+        
+        # Initialize layers
+        self.transformer = Transformer(d_model=d_model,
+                                       nhead=nhead,
+                                       num_encoder_layers=num_encoder_layers,
+                                       num_decoder_layers=num_encoder_layers,
+                                       dim_feedforward=dim_feedforward,
+                                       dropout=dropout,
+                                       batch_first=True) 
+        
+        self.latentEncoder = nn.Linear(maxlen*d_model, self.d_latent) 
+        self.linear = nn.Linear(self.d_latent, 1)
+        self.sigmoid = nn.Sigmoid()
+        
+        # Initialize helper modules
+        self.src_embedding = VarSeriesEmbedding(num_src_variables, d_model)   
+        self.positional_encoding = PositionalEncoding(d_model)
+        
+    def init_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+                
+    def forward(self,
+               src_pad: Tensor,
+               src_mask: Tensor = None, 
+               src_padding_mask: Tensor = None):
+        bz, maxlen, _ = src_pad.shape
+        src_emb = torch.zeros((bz, maxlen, self.d_model),device=DEVICE) 
+            
+        # Encode input batch into the embedding
+        for batch in range(bz):
+            src_emb[batch,:] = torch.stack([self.src_embedding(i) for i in src_pad[batch]])
+        
+        # Add positional encoding
+        src_emb = self.positional_encoding(src_emb)
+
+        # Get Transformer-Encoder output
+        memory = self.transformer.encoder(src = src_emb,
+                                          mask = src_mask,
+                                          src_key_padding_mask = src_padding_mask)
+        # Create Bottleneck
+        memory = self.latentEncoder(memory.reshape((bz, self.maxlen*self.d_model)))   
+        
+        # Get prediction
+        logits = self.linear(memory)
+        p = self.sigmoid(logits)
+        
+        #logits.retain_grad()
+        
+        return logits, p
